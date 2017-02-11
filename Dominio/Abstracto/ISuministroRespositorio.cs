9c9a66e43a5d8b27de0f0315ef3204525dce4a1a
GDESUR <GDESUR@DESKTOP-NLSL1NR>
2017-02-10 19:08:25 -0600

    Actualizacion nueva

---
+++ Dominio/Abstracto/ISuministroRespositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace Dominio.Abstracto
+{
+    class ISuministroRespositorio
+    {
+        IEnumerable<Articulos> Suministros { get; set; }
+    }
+}
---
+++ WebUI/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(WebAdminUser usr)
+        {
+            //var errors = ModelState.Values.SelectMany(v => v.Errors);
+            if (ModelState.IsValid)
+            {
+                using (InformaticaEntities context = new  InformaticaEntities())
+                {
+                 
+                    var obj = context.WebAdminUser.Where(x => x.Correo.Equals(usr.Correo) && x.contraseña.Equals(usr.contraseña)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        return RedirectToAction("UserDashBoard");
+                    }
+
+                }
+            }
+            TempData["message"] = "Usuario ó contraseña invalida";
+            return View(usr);
+           
+        }
+
+
+    }
+}
