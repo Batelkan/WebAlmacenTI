1b035afcdf4d9056cac69540bfd89369b453f4a0
GDESUR <GDESUR@DESKTOP-NLSL1NR>
2017-03-28 19:01:13 -0600

    Lista Computo

--- WebUI/Areas/AreaComputadoras/Controllers/ComputadorasController.cs
+++ WebUI/Areas/AreaComputadoras/Controllers/ComputadorasController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using Dominio.Concreto;
 using Dominio.Abstracto;
 using Dominio.Modelo;
+using WebUI.Areas.AreaComputadoras.Models;
+using WebUI.Infraestructura1;
 
 namespace WebUI.Areas.AreaComputadoras.Controllers
 {
@@ -25,9 +27,37 @@ namespace WebUI.Areas.AreaComputadoras.Controllers
             repositorio = repo;
         }
 
-        public ViewResult ComputadorasLista(int pagina = 1,int Numitems = 15,string orden="asc")
+        public ViewResult ComputadorasLista( int pagina = 1, int Numitems = 15, string orden = "asc" )
         {
+            ComputadoraViewModel modelo = new ComputadoraViewModel()
+            {
+                PaginaInfo = new WebUI.Models.PaginacionInfo
+                {
+                    PaginaActual = pagina,
+                    ItemPorPagina = Numitems,
+                    ItemsTotales = repositorio.Computo.Count()
+            
+                },
+                itemsPorPagina = Numitems,
+                OrdenFecha = orden
+                
+            };
 
+            if(orden == "asc")
+            {
+                modelo.ListaComputadoras = repositorio.Computo
+                    .OrderBy(c => c.FechaAlta)
+                    .Skip((pagina - 1) * Numitems)
+                    .Take(Numitems);
+            }
+          else
+            {
+                modelo.ListaComputadoras = repositorio.Computo
+                  .OrderByDescending(c => c.FechaAlta)
+                  .Skip((pagina - 1) * Numitems)
+                  .Take(Numitems);
+            }
+            return View(modelo);
         }
 
     }
--- WebUI/Areas/AreaComputadoras/Models/ComputadoraViewModel.cs
+++ WebUI/Areas/AreaComputadoras/Models/ComputadoraViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Dominio.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Dominio.Modelo;
 using WebUI.Models;
 
 namespace WebUI.Areas.AreaComputadoras.Models
